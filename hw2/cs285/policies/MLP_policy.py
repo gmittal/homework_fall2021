cb910a7b0db207a384107b1887703ea80df0ea28
Ubuntu <ubuntu@ip-172-31-4-105.us-west-1.compute.internal>
2021-09-26 08:27:34 +0000

    Init HW2

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -86,7 +86,19 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
-        # TODO: get this from HW1
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        observation = torch.tensor(observation).to(ptu.device)
+        if self.discrete:
+            x = self.forward(observation)
+        else:
+            dist = self.forward(observation)
+            x = dist.sample()
+
+        return ptu.to_numpy(x)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
